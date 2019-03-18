2a6ea00379e41836fee852bb9f875554a6b4bd9b
springleeo@163.com <springleeo@springleeodeMacBook-Pro.local>
2019-03-18 23:41:21 +0800

    v-model

--- src/components/vmodel.vue
+++ src/components/vmodel.vue
@@ -8,15 +8,32 @@
     </p>
     <span>multiline message is:</span>
     <p style="white-space: pre-line;">{{ msg }}</p>
-    <textarea v-model="msg" cols="30" rows="10" placeholder="add multiline message"></textarea>
+    <textarea v-model="msg" cols="30" rows="2" placeholder="add multiline message"></textarea>
+    <p>单选框绑定：</p>
     <p>
-      <input type="radio" v-model="checked">
-      <label for="radio">{{checked}}</label>
+      <input type="radio" name="" id="fale" value="男" v-model="sex">
+      <label for="fale">男</label>
     </p>
     <p>
-      <input type="checkbox" v-model="checked">
-      <label for="checkbox">{{checked}}</label>
+      <input type="radio" name="" id="female" value="女" v-model="sex">
+      <label for="female">女</label>
     </p>
+    <p>性别是：{{sex}}</p>
+    <p>多选框绑定数组：</p>
+    <p>
+      <input type="checkbox" id="java" value="java" v-model="lang">
+      <label ffor="isTrue">java</label>
+      </p>
+    <p>
+      <input type="checkbox" id="python" value="python" v-model="lang">
+      <label for="isTrue">python</label>
+      </p>
+    <p>
+      <input type="checkbox" id="vue" value="vue" v-model="lang">
+      <label for="isTrue">vue</label>
+      </p>
+    <p>语言是：{{lang}}</p>
+
   </div>
 </template>
 
@@ -26,7 +43,9 @@ export default {
   data() {
     return {
       msg: '',
-      checked: true
+      sex: '',
+      lang: [],
+      isTrue: true,
     }
   }
 }
