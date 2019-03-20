48cbcf0cb86b443577853ec1d9ad4803a111de95
springleeo <springleeo@163.com>
2019-03-20 16:28:32 +0800

    axios post

---
+++ src/components/axiosPost.vue
@@ -0,0 +1,27 @@
+<template></template>
+
+<script>
+export default {
+  name: 'axiosPost',
+  data() {
+    return {
+
+    }
+  },
+  created() {
+    this.$axios.post("", {
+      user: '',
+      password: ''
+    })
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
+
+<style>
+</style>
